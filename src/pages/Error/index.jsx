7998afbcaef576fbee6df9f8d2fee211d7162cab
feruzbek715 <nazirjon502@gmail.com>
2024-05-02 21:42:03 +0500

    error page terminated

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -5,16 +5,17 @@ function Error() {
   const navigate = useNavigate();
   return (
     <div className="flex-container">
-      <div className="text-center">
-        
-        <button className="btn btn-primary"
+      <div className="text-center ">
+        <h1 className="text-8xl text-[red] font-bold">404 Not Found</h1>
+        <p className="text-5xl text-[red] mb-8">you entered the wrong url</p>
+        <button className="btn btn-primary w-[190px]"
           type="button"
           name="button"
           onClick={() => {
             navigate("/");
           }}
         >
-          Return Home
+          Return Home Pages
         </button>
       </div>
     </div>
